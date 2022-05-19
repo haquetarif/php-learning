307a4fd0614733e63f25da4df8f00a1e25bb304b
Tarif Haque <tarif@poems.codes>
2022-05-19 17:32:35 +0600

    adds a QueryBuilder class and refractor the by using its instances

--- functions.php
+++ functions.php
@@ -1,22 +1,6 @@
 <?php
 
 
-
-function fetchAllTasks($pdo)
-
-{
-
-	$statement = $pdo->prepare('select * from todos');
-
-	$statement->execute();
-
-
-	return $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
-
-
-	
-}
-
 function dd($value) {
 
 		
@@ -27,19 +11,6 @@ function dd($value) {
 		echo '</pre>';
 	}
 
-function checkAdulthood($age) {
 
-	if($age >= 21) {
-		
-		echo 'Come on in!';
-	
-	} else {
-
-		echo 'You are not old enough! Sorry!';
-
-
-	}
-
-}
 
 ?>
--- index.php
+++ index.php
@@ -1,14 +1,29 @@
 <?php
 
 
-require 'functions.php';
 require 'database/Connection.php';
-require 'Task.php';
 
-$pdo = Connection::make();
+require 'database/QueryBuilder.php';
 
-$tasks = fetchAllTasks($pdo);
+// require 'Task.php';
 
+
+
+$pdo = Connection::make(); /* mySQL connection is made with 
+
+the static function "make()" from the Connection.php file */
+
+
+
+$query = new QueryBuilder($pdo); /* New "query" object is created 
+
+based on the QueryBuilder class from the PHP file with the same name */
+
+
+
+$tasks = $query->selectAll('todos'); /* New "tasks" object is created
+
+by fetching data from the 'todos' table of the mytodo database */
 
 
 
